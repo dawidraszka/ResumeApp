f57c96896afd0f2a15d25f2564956333f524a86c
Dawid <raszka.dawid@gmail.com>
2021-03-26 19:16:20 +0100

    Added simple progress bar while loading images

--- app/src/main/java/pl/dawidraszka/resumeapp/ui/projects/adapters/ImageListAdapter.kt
+++ app/src/main/java/pl/dawidraszka/resumeapp/ui/projects/adapters/ImageListAdapter.kt
@@ -3,6 +3,7 @@ package pl.dawidraszka.resumeapp.ui.projects.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_image.view.*
 import pl.dawidraszka.resumeapp.R
@@ -38,7 +39,13 @@ class SimpleListViewHolder(
 ) :
     RecyclerView.ViewHolder(inflater.inflate(R.layout.item_image, parent, false)) {
 
+
     fun bind(screenUrl: String) {
+        val circularProgressDrawable = CircularProgressDrawable(itemView.context)
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.start()
+
         Glide.with(itemView)
             .load(screenUrl)
             .into(itemView.image_view)
--- app/src/main/java/pl/dawidraszka/resumeapp/ui/projects/adapters/ProjectsListAdapter.kt
+++ app/src/main/java/pl/dawidraszka/resumeapp/ui/projects/adapters/ProjectsListAdapter.kt
@@ -3,6 +3,7 @@ package pl.dawidraszka.resumeapp.ui.projects.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_project.view.*
 import pl.dawidraszka.resumeapp.R
@@ -40,15 +41,23 @@ class ProjectViewHolder(
 
     fun bind(project: Project) {
         itemView.apply {
+            val circularProgressDrawable = CircularProgressDrawable(context)
+            circularProgressDrawable.strokeWidth = 5f
+            circularProgressDrawable.centerRadius = 30f
+            circularProgressDrawable.start()
+
             with(project) {
                 Glide.with(context)
                     .load(screenshots?.get(0))
+                    .placeholder(circularProgressDrawable)
                     .into(screenshot_1)
                 Glide.with(context)
                     .load(screenshots?.get(1))
+                    .placeholder(circularProgressDrawable)
                     .into(screenshot_2)
                 Glide.with(context)
                     .load(screenshots?.get(2))
+                    .placeholder(circularProgressDrawable)
                     .into(screenshot_3)
                 name_text_view.text = name
                 description_text_view.text = description
